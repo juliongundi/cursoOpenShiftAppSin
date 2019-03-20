2856d0ff795dca19732a4d8393ee497cd3dd2f8e
juliongundi <48751621+juliongundi@users.noreply.github.com>
2019-03-20 10:19:40 +0100

    cambio

--- src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
+++ src/main/java/com/inigoserrano/openshift/aplicacion/Aplicacion.java
@@ -24,8 +24,8 @@ public class Aplicacion extends HttpServlet {
 
 	@Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("Salida salida salida otra vez");
-        response.getWriter().println("Salida GET webwebweb otra vez");  
+        System.out.println("Entrada salida otra vez");
+        response.getWriter().println("Salida y entrada GET webwebweb otra vez");  
         response.getWriter().flush();
     }
 
